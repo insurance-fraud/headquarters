287fe6cfc7673fc3093069863ebdf6ef230a591f
Nikola Đuza <nikolaseap@gmail.com>
2019-06-29 15:30:38 -0500

    Add test for paying for insurance

---
+++ cypress/integration/pay_for_insurance.js
@@ -0,0 +1,22 @@
+describe('Paying for insurance', function() {
+  it('Cheapest insurance', function() {
+    cy.visit('http://localhost:3000')
+
+    cy.contains('Welcome to merch-it')
+
+    cy.get('[data-cy=email]').type('test@example.com')
+    cy.get('[data-cy=10000]').click()
+
+    cy.contains('Checkout')
+    cy.get('[data-cy=submit]').click()
+
+    cy.get('[data-cy=pan]').type('1')
+    cy.get('[data-cy=securityCode]').type('1')
+    cy.get('[data-cy=cardHolderName]').type('1')
+    cy.get('[data-cy=expirationDate]').type('1')
+
+    cy.get('[data-cy=pay]').click()
+
+    cy.contains("Congrats, you've bought insurance from us!")
+  })
+})
