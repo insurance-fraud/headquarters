08d8f84ef47bff4fdf3d1138c6ce4a9adce5b454
Nikola Đuza <nikolaseap@gmail.com>
2019-07-30 18:28:55 +0200

    Add Nightwatch example test

---
+++ nightwatch/google.js
@@ -0,0 +1,17 @@
+module.exports = {
+  tags: ['google'],
+
+  'Demo test Google' : function (client) {
+    client
+      .url('http://google.no')
+      .waitForElementVisible('body', 1000);
+
+    client.expect.element('body').to.be.present;
+
+    client.expect.element('body').to.have.attribute('class').which.contains('vasq');
+    client.expect.element('body').to.have.attribute('class').which.matches(/vasq$/);
+    client.expect.element('body').to.have.attribute('class').before(1000);
+
+    client.end();
+  }
+};
